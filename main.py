94301aedc46ccb1f8fdb5a9f3f93fe107024e717
Zi <zinaabdel345@outlook.fr>
2021-06-10 17:09:22 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,13 @@
+# Import de flask
+# Encore un commentaire
+"""Super fichier avec des trucs dedans"""
+
+from flask import Flask
+
+app = Flask(__name__)
+@app.route('/')
+def index():
+    return "Hello world !"
+
+if __name__ == "__main__":
+    app.run()
