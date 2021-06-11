4499043f4ce8fc98b2b3ef7abff734ba40a1cc70
Zi <zinaabdel345@outlook.fr>
2021-06-11 09:16:57 +0200

    Add comment Again

--- main.py
+++ main.py
@@ -4,11 +4,14 @@
 """Super fichier avec des trucs dedans"""
 
 from flask import Flask
-
+"""Super fichier avec des trucs dedans"""
 app = Flask(__name__)
+"""Super fichier avec des trucs dedans"""
 @app.route('/')
+"""Super fichier avec des trucs dedans"""
 def index():
-    return "Hello world !"
-
+    return "Hello world !" """Super fichier avec des trucs dedans"""
+"""Super fichier avec des trucs dedans"""
 if __name__ == "__main__":
+    """Super fichier avec des trucs dedans"""
     app.run()
